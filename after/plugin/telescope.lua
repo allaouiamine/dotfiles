f50e6c10c46d89c134184135538b0db63eee0236
Amine Allaoui <mohamed.amine@klarna.com>
2023-12-11 10:21:38 +0100

    ++ telescope exception

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,6 @@
 local telescope = require('telescope')
 local builtin = require('telescope.builtin')
-telescope.setup{ defaults = {file_ignore_patterns = {"./target/*"}, }}
+telescope.setup{ defaults = {file_ignore_patterns = {"./target/*", "./.tox/*"}, }}
 telescope.setup{ defaults = {file_ignore_patterns = {"./venv/*"}, }}
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
