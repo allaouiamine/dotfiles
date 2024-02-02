aae7cf2027cc79887e0ddd93884918bb2787f2e8
Amine Allaoui <mohamed.amine@klarna.com>
2024-02-02 08:36:49 +0100

    bug fix on install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,15 @@
 
 parent_dir=$(dirname "$(readlink -f "$0")")
 
-ln -s ${parent_dir}/neovim ~/.config/nvim 2>/dev/null
-ln -s ${parent_dir}/dotfiles/alacritty ~/.config/alacritty 2>/dev/null
-ln -s ${parent_dir}/dotfiles/tmux/tmux.conf ~/.tmux.conf 2>/dev/null
+nvim_cmd="ln -s ${parent_dir}/neovim ~/.config/nvim 2>/dev/null"
+echo "Linking neovim config ${nvim_cmd}"
+eval "${nvim_cmd}"
+
+alacritty_cmd="ln -s ${parent_dir}/alacritty ~/.config/alacritty 2>/dev/null"
+echo "Linking alacritty config ${alacritty_cmd}"
+eval "${alacritty_cmd}"
+
+tmux_cmd="ln -s ${parent_dir}/tmux/tmux.conf ~/.tmux.conf 2>/dev/null"
+echo "Linking tmux config ${tmux_cmd}"
+eval "${tmux_cmd}"
+
