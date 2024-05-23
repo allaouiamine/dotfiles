a5f049f1806a3b0fa5a80f989939fbb1845105b3
amine.allaoui <amine.allaoui@aiven.io>
2024-05-23 15:54:19 +0200

    fixed gen.nvim config

--- neovim/after/plugin/gen.lua
+++ neovim/after/plugin/gen.lua
@@ -1,11 +1,9 @@
+
 require('gen').setup({
-    opts = {
         model = "llama3", -- The default model to use.
         host = "ollama", -- The host running the Ollama service.
         show_model = true, -- Displays which model you are using at the beginning of your chat session.
-        show_prompt = true
-    },
-    --prompts=
+        show_prompt = true,
 })
 
 
