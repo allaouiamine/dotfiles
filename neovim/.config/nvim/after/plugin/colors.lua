78383a73fa4665a4a8bb33f0bb27bc4aab576042
Amine Allaoui <hello@amineallaoui.com>
2024-11-14 13:42:59 +0100

    fixed background color for notify in neovim

--- neovim/.config/nvim/after/plugin/colors.lua
+++ neovim/.config/nvim/after/plugin/colors.lua
@@ -1,6 +1,10 @@
-function ColorMyPencils(color)
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+function ColorMyPencils()
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
+
+require("notify").setup({
+    background_color = "#1a1b26",
+})
 
 ColorMyPencils()
